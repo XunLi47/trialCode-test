3affd80f9d229e2c1023d12caedf5dae6da10676
XunLi47 <lixun0714@163.com>
2020-12-22 10:54:52 +0800

    解决日志问题

--- src/main/java/com/trial/dms/trialCode/test/Dome.java
+++ src/main/java/com/trial/dms/trialCode/test/Dome.java
@@ -8,7 +8,13 @@ public class Dome {
 	private static final Logger logger  = LoggerFactory.getLogger(Dome.class);
 	
 	public static void main(String[] args) {
-		logger.info("qweqwe");
+		try {
+			System.out.println("123123");
+			String a = "按时打算打发大水发个";
+			logger.info("adfgSDFSBqweqwe：{}",a);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 	
 
