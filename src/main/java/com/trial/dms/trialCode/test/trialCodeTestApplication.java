c142d7dd67356dd1d25d37f313d0c8e49b382e75
XunLi47 <lixun0714@163.com>
2020-12-22 14:21:00 +0800

    1

--- src/main/java/com/trial/dms/trialCode/test/trialCodeTestApplication.java
+++ src/main/java/com/trial/dms/trialCode/test/trialCodeTestApplication.java
@@ -1,13 +1,16 @@
 package com.trial.dms.trialCode.test;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.trial.dms")
+@SpringBootApplication
+@EnableAutoConfiguration
 public class trialCodeTestApplication 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+    	SpringApplication.run(trialCodeTestApplication.class,args);
     }
 }
